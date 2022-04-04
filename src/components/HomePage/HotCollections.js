3e75bbd3f32df14c3c5b27989d29454c51f072d3
nahom-habtamu <cnetappeth@gmail.com>
2022-04-04 08:09:30 +0300

    Extracted Out HotCollectionItem from HomePage

--- src/components/HomePage/HotCollections.js
+++ src/components/HomePage/HotCollections.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import HotCollectionItem from './HotCollectionItem';
 import HotCollectionsTitle from './HotCollectionsTitle';
 
 const HotCollections = () => {
@@ -34,30 +34,6 @@ const HotCollections = () => {
     </section>
   )
 }
-
-const HotCollectionItem = () => {
-    return (
-        <div className="collection">
-            <Link 
-                to="/collection" 
-                className="collection__cover" 
-                data-bg="img/bg/bg-small.png">
-            </Link>
-            <div className="collection__meta">
-                <Link 
-                    to='/author' 
-                    className="collection__avatar collection__avatar--verified">
-                    <img src="img/avatars/avatar3.jpg" alt=""/>
-                </Link>
-                <h3 className="collection__name">
-                    <Link to='collection'>Hashmasks</Link>
-                </h3>
-                <span className="collection__number">ERC-721</span>
-            </div>
-        </div>
-    );
-}
-
 
 export default HotCollections;
 
