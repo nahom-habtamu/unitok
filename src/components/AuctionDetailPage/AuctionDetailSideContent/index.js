7c97001984d033af16556581e2f4aa9a3ada6bd7
nahom-habtamu <cnetappeth@gmail.com>
2022-04-04 09:06:46 +0300

    Fix Problems Regarding BidModal Events By Creating a Portal

--- src/components/AuctionDetailPage/AuctionDetailSideContent/index.js
+++ src/components/AuctionDetailPage/AuctionDetailSideContent/index.js
@@ -6,24 +6,41 @@ import AuctionDetailTabs from './AuctionDetailTabs';
 import AuctionDetailTabsContent from './AuctionDetailTabsContent';
 import AssetBottomContent from './AssetBottomContent';
 import BidModal from './BidModal';
+import Portal from '../../common/Portal';
 
 const AuctionDetailSideContent = () => {
 
   const [bid, setBid] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className='col-12 col-xl-4'>
-        <div className='asset__info'>
-            <AuctionDetailDescription/>
-            <AuthorAsset/>
-            <AuctionDetailTabs tabs={["History","Bids"]}/>
-            <AuctionDetailTabsContent/>
-            <AssetBottomContent/>
-            <BidModal 
+      <div className='asset__info'>
+        <AuctionDetailDescription />
+        <AuthorAsset />
+        <AuctionDetailTabs tabs={["History", "Bids"]} />
+        <AuctionDetailTabsContent />
+        <AssetBottomContent
+          onShowModalClicked={() => setShowModal(true)}
+        />
+        {
+          showModal &&
+
+          <Portal>
+            <BidModal
+              onCloseModal={() => setShowModal(false)}
+              bid = {bid}
               onBidChanged={(value) => setBid(value)}
-              onBidSubmitted={() => console.log(bid)}
+              onBidSubmitted={() => {
+                console.log(bid);
+                setShowModal(false);
+              }}
+              isActive={showModal}
             />
-        </div>
+          </Portal>
+
+        }
+      </div>
     </div>
   );
 }
